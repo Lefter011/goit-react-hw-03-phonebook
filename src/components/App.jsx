f8f5adb40ecf7e77675162ca50d1c8dbc57c263d
Mike Chernetskyi <michael.chernetskyi@gmail.com>
2022-12-29 14:20:11 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,23 @@ import { ContactsForm } from "./ContactsForm/ContactsForm";
 import { FilterInput } from "./FilterForm/FilterInput";
 import style from './app.module.css';
 
+const CONTACTS_KEY = 'CONTACTS_KEY';
+
 export class App extends Component {
   state = {
-    contacts: [
-    { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-    {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-    {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-    { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
+  }
+
+    componentDidMount() {
+    const savedContacts = JSON.parse(localStorage.getItem(CONTACTS_KEY));
+    savedContacts && this.setState(({ contacts: savedContacts }));
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem(CONTACTS_KEY, JSON.stringify(this.state.contacts));
+    }
   }
   
   onFormSubmit = (e) => {
@@ -44,7 +52,7 @@ export class App extends Component {
   }
 
   onFilterInput = (e) => {
-    this.setState({ filter: e.currentTarget.value } )
+    this.setState({ filter: e.currentTarget.value })
   }
 
   getSearchContact = () => {
